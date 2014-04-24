f5a24b93abf74090c1fcbadf79090189631e244c
mtomoki <dezaley22@gmail.com>
2014-04-24 12:43:39 +0900

    課題用UPデート
    clickボタン増設

--- js/helloworld.js
+++ js/helloworld.js
@@ -2,6 +2,17 @@ var helloWorld = function(){
 	var message = document.getElementById("helloWorld");
 	message.setAttribute("class", "");
 };
-
 var showHelloWorldButton = document.getElementById("showHelloWorld");
 showHelloWorldButton.addEventListener("click", helloWorld);
+
+
+
+
+var hazure = function(){
+	var message = document.getElementById("hazure");
+	message.setAttribute("class", "");
+};
+var showHazureButton = document.getElementById("showHazure");
+showHazureButton.addEventListener("click", hazure);
+
+
